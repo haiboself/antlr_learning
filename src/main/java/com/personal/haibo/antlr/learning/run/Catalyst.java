eae21f47d4bfe1e4b6dbe4497ff42e5bd4d3573d
haibo <haibo@meituan.com>
2020-09-28 20:11:31 +0800

    fix catalyst

--- src/main/java/com/personal/haibo/antlr/learning/run/Catalyst.java
+++ src/main/java/com/personal/haibo/antlr/learning/run/Catalyst.java
@@ -8,14 +8,9 @@ import org.apache.spark.sql.internal.SQLConf;
 
 public class Catalyst {
     public static void main(String[] args) {
-        String sql = "create table x.y as select * from a.b";
+        String sql = "select * from a.b";
         CatalystSqlParser parser = new CatalystSqlParser(new SQLConf());
-        SqlBaseParser.CreateHiveTableContext tree = (SqlBaseParser.CreateHiveTableContext) parser.parse(sql, SqlBaseParser::statement);
-        SqlBaseParser.QueryContext query = tree.query();
-        int start = query.getStart().getStartIndex();
-        int stop = query.getStop().getStopIndex()+1;
-
-        String tmpTb = "create table h.h as " + sql.substring(start, stop);
+        ParserRuleContext tree = parser.parse(sql, SqlBaseParser::singleStatement);
         LogicalPlan logicalPlan = (LogicalPlan) parser.astBuilder().visit(tree);
         System.out.println(logicalPlan);
     }
